f4f40eed36c33f869069cd0e4c69185fd7f24381
tikoot <tiko.otarashvili99@gmail.com>
2022-09-10 21:04:44 +0200

    feat:create user register request

---
+++ app/Http/Requests/RegisterUserRequset.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequset extends FormRequest
+{
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function rules()
+	{
+		return [
+			'username'              => 'required|min:3',
+			'email'                 => 'required|email',
+			'password'              => 'required|min:3',
+			'password_confirmation' => 'required|min:3|same:password',
+		];
+	}
+
+	public function messages()
+	{
+		return [
+			'username.required'          => 'Username is required',
+			'username.min'               => 'Too Short',
+			'email.required'             => 'Email is required',
+			'password.required'          => 'A message is required',
+			'password.min'               => 'Too Short',
+			'password_confirmation.same' => 'Password Confirmation should match the Password',
+		];
+	}
+}
