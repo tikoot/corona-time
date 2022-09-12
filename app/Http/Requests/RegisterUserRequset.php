f13ef2af24c1d61521364772627e18f10fc05fde
tikoot <tiko.otarashvili99@gmail.com>
2022-09-12 06:50:53 +0200

    feat:create custom verification email

--- app/Http/Requests/RegisterUserRequset.php
+++ app/Http/Requests/RegisterUserRequset.php
@@ -14,8 +14,8 @@ class RegisterUserRequset extends FormRequest
 	public function rules()
 	{
 		return [
-			'username'              => 'required|min:3',
-			'email'                 => 'required|email',
+			'username'              => 'required|min:3|unique:users',
+			'email'                 => 'required|email|unique:users',
 			'password'              => 'required|min:3',
 			'password_confirmation' => 'required|min:3|same:password',
 		];
--- resources/views/auth/confirm-email.blade.php
+++ resources/views/auth/confirm-email.blade.php
@@ -1,10 +1,8 @@
 <x-layout>
-    <div class="flex flex-col justify-center items-center m-auto h-screen max-w-sm">
-        
+    <div class="flex flex-col justify-center items-center m-auto h-screen max-w-sm">        
        <x-logo/>
-
         <div class='flex flex-col items-center justify-center  m-auto'>
-            <img src="{{asset('storage/assets/icons8-checked.png')}}" class="pb-4 " alt="checkmark">
+            <x-verify.checkmark/>
             <p class="pb-24">{{__('register.your_account_is_confirmed_you_can_sign_in')}}</p>
             <a href="" class="w-full py-3 text-center text-lg rounded-lg text-white bg-green"> {{__('login.log_in')}}</a>
         </div>
