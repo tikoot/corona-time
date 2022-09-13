f1015ab97f61d9964833b85358cbac14bb2cb6d3
tikoot <tiko.otarashvili99@gmail.com>
2022-09-13 16:46:27 +0200

    feat:add password reset markup and send reset email

---
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ForgotPasswordController extends Controller
+{
+	public function sendResetLink(Request $request): View|RedirectResponse
+	{
+		$request->validate(['email' => 'required|email']);
+
+		$status = Password::sendResetLink(
+			$request->only('email')
+		);
+
+		return $status === Password::RESET_LINK_SENT
+		? view('auth.email.verify-email')
+		: back()->withErrors(['email' => __($status)]);
+	}
+}
