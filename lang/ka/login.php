ee95df0ab7763950646311d10dd4b9ff2adbcb7d
tikoot <tiko.otarashvili99@gmail.com>
2022-09-15 00:05:58 +0200

    feat:add translations

--- lang/ka/login.php
+++ lang/ka/login.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-	'log_in'                                   => 'შესვლა',
-	'welcome_back'                             => 'კეთილი იყოს თქვენი დაბრუნება!',
-	'welcome_back_please_enter_your_details'   => 'კეთილი იყოს თქვენი დაბრუნება! გთხოვთ შეიყვანოთ თქვენი დეტალები',
-	'don’t_have_an_account?'                   => ' არ გაქვთ ანგარიში?',
-	'forgot_password?'                         => 'დაგავიწყდათ პაროლი?',
-	'reset_password'                           => 'პაროლის აღდგენა',
-	'save_changes'                             => 'ცვლილებების შენახვა',
+	'log_in'                                     => 'შესვლა',
+	'welcome_back'                               => 'კეთილი იყოს თქვენი დაბრუნება!',
+	'welcome_back_please_enter_your_details'     => 'კეთილი იყოს თქვენი დაბრუნება! გთხოვთ შეიყვანოთ თქვენი დეტალები',
+	'don’t_have_an_account?'                     => ' არ გაქვთ ანგარიში?',
+	'forgot_password?'                           => 'დაგავიწყდათ პაროლი?',
+	'reset_password'                             => 'პაროლის აღდგენა',
+	'save_changes'                               => 'ცვლილებების შენახვა',
+	'sign_in'                                    => 'შესვლა',
+	'your_password_has_been_updeted_successfully'=> 'თქვენი პაროლი წარმატებით განახლდა',
 ];
