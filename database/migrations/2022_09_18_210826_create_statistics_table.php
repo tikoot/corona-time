71ea8cb5225a46010b4c2643ac95782706d4f9d2
tikoot <tiko.otarashvili99@gmail.com>
2022-09-19 01:32:12 +0200

    feat:create covid statistics table and model

---
+++ database/migrations/2022_09_18_210826_create_statistics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('statistics', function (Blueprint $table) {
+			$table->id();
+			$table->string('country');
+			$table->json('code');
+			$table->integer('confirmed');
+			$table->integer('recovered');
+			$table->integer('critical');
+			$table->integer('deaths');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('statistics');
+	}
+};
