9d2d3972a85e6bea39507560696afe1163d181ea
tikoot <tiko.otarashvili99@gmail.com>
2022-09-20 16:43:24 +0200

    feat:create search by country name

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Statistic;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-	public function CountryStatistics(): View
+	public function CountryStatistics(Request $request): View
 	{
+		$search = $request->search;
+		if ($search)
+		{
+			$statistics = Statistic::where('country->ka', 'LIKE', "%{$search}%")
+			->orWhere('country->en', 'LIKE', '%' . ucwords($search) . '%')
+			->get();
+
+			return view('countries', [
+				'statistics' => $statistics,
+			]);
+		}
+
 		return view('countries', [
 			'statistics' => Statistic::all(),
 		]);
--- resources/views/countries.blade.php
+++ resources/views/countries.blade.php
@@ -13,9 +13,25 @@
                 > {{__('dashboard.by_country')}}
             </a>
       </div>
-    
+      
       <main class="pt-10">
-        <div class="max-w-full py-10">
+        
+    
+        <form class="relative pb-10"> 
+          <div class="absolute top-[14px] left-6">
+              <img src="{{asset('/storage/assets/Vector.png')}}" alt="">
+          </div>
+        <input type="search" value="{{request('search')}}" name="search" class="h-12 w-64 pl-14 pr-8 text-[15px] text-gray font-medium rounded-lg z-0 border border-lightgray" placeholder="Search by country">
+          <div class="absolute top-2 right-2">
+              
+              <button class="h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600">Search</button>
+              
+          </div>
+        </form>
+
+    
+  
+        <div class="max-w-full ">
           <div class="overflow-x-hidden  text-left shadow ring-1 ring-grey md:rounded-lg">
             <table class="w-full table-fixed divide-y divide-grey text-sm">
               <thead class="bg-grey">
